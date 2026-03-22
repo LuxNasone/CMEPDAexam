e0886ce90bba814a6918d83144f325b1bedeeb2e
LuxNasone <nasoneluca@gmail.com>
2026-03-22 16:45:00 +0100

    commit di controllo

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,32 @@
 #include <TFile.h>
 #include <TTree.h>
 
-void main(const char* fname){
+void console(const char* fname){
 
-    //Path must be to a .root file
+    //Path must be to a .root file, also a check
 
-    TFile *f = TFile::Open(path);
+    TFile *f = TFile::Open(fname);
 
-    //TTree initialization
+    if (!f || f->IsZombie()) { 
+        std::cerr << "Error: file not found or corrupted\n"; 
+        return;
+    }
 
-    TTree *t = (TTree*)f->Get("T");
+    //TTree initialization, also a check
+
+    TTree *t = (TTree*)f->Get("Events");
+
+    if (!t) {
+        std::cerr << "Error: tree not found in file\n";
+        return;
+    }
+
+    //Control : plotting Muon_pt histogram
+
+    TH1F *hPT = new TH1F("hPT", "Muon_pt", 100, -50, 200);
+
+    t->Draw("Muon_pt >> hPT");
+
+    hPT->Draw();
 
 }
