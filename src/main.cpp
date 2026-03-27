ba8d17ba4d725d3c8cb2da5afe0c0a83be64441d
LuxNasone <nasoneluca@gmail.com>
2026-03-27 10:20:30 +0100

    Sistemati i grafici, aggiunti argomenti del Main, implementato MT

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <chrono>
 
 #include <TSystem.h> 
 #include <TFile.h>
@@ -11,17 +12,18 @@
 #include <TLorentzVector.h>
 #include <ROOT/RDataFrame.hxx>
 
-void Main(const char* fname){
+void Main(const char* fname, std::string outname){
 
     //Necessary imports for 4-vectors, compile macro with + at the end
 
     gSystem->Load("libPhysics"); 
 
-    //Import necessary to save graphs
+    //Chrono counter
 
-    std::string outdir = std::string(getenv("HOME")) + "/CMEPDA/Exam/Repo/graphs/";
+    auto start = std::chrono::high_resolution_clock::now();
 
-    gSystem->mkdir(outdir.c_str(), true);
+    //Activating parallel execution by multithreading (kind of a black box ... maybe put it as an option?)
+    ROOT::EnableImplicitMT();
 
     //Initializing DataFrame, fname must be to a .root file
 
@@ -47,10 +49,15 @@ void Main(const char* fname){
                               "Muon_pt[0] > 25 && Muon_pt[1] > 25 &&"
                               "abs(Muon_eta[0]) < 2.4 && abs(Muon_eta[1]) < 2.4")
                       .Define("Dimuon_mass", [](const ROOT::RVecF &pt, const ROOT::RVecF &eta, const ROOT::RVecF &phi, const ROOT::RVecF &mass){
-                               ROOT::Math::PtEtaPhiMVector p_1(pt[0],eta[0],phi[0], mass[0]);
-                               ROOT::Math::PtEtaPhiMVector p_2(pt[1],eta[1],phi[1], mass[1]);
+                               ROOT::Math::PtEtaPhiMVector p_1(pt[0],eta[0],phi[0], mass[0]), p_2(pt[1],eta[1], phi[1], mass[1]);
                                return (Float_t)(p_1 + p_2).M();}, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"})
-                      .Filter("abs(Dimuon_mass - 91.1817) < 15");
+                      .Filter("abs(Dimuon_mass - 91.1817) < 15")
+                      .Define("Muon0_pt",  "Muon_pt[0]")
+                      .Define("Muon1_pt",  "Muon_pt[1]")
+                      .Define("Muon0_eta", "Muon_eta[0]")
+                      .Define("Muon1_eta", "Muon_eta[1]")
+                      .Define("Muon0_phi", "Muon_phi[0]")
+                      .Define("Muon1_phi", "Muon_phi[1]");
 
     //Histogram for Invariant Mass
 
@@ -58,31 +65,48 @@ void Main(const char* fname){
     TCanvas *c_dmm = new TCanvas("M_inv", "DiMuon_mass_canvas", 800, 600);
     h_dmm->Draw();
     c_dmm->Update();
-    c_dmm->SaveAs((outdir + "DiMuonMass.png").c_str());
+    c_dmm->SaveAs((outname+"Mass.png").c_str());
 
-    //Histogram for transverse momentum
+    delete c_dmm;
 
-    auto h_pt = new_df.Histo1D({"Pt", "Transverse_momentum", 100, 25, 200}, "Muon_pt");
-    TCanvas *c_pt = new TCanvas("Pt", "Transverse_momentum_canvas", 800, 600);
-    h_pt->Draw();
-    c_pt->Update();
-    c_pt->SaveAs((outdir +"DiMuonPT.png").c_str());
+    //Variables necessary for loop
 
-    //Histogram for pseudorapidity
+    std::vector<std::string> vars = {"pt", "eta", "phi"};
+    std::vector<std::pair<Float_t, Float_t>> bounds = {{15, 200},
+                                              {-3, 3},
+                                              {-4, 4}};
 
-    auto h_eta = new_df.Histo1D({"Eta", "Muon_pseudorapidity", 100, -2.4, 2.4}, "Muon_eta");
-    TCanvas *c_eta = new TCanvas("Eta", "Pseudorapidity_canvas", 800, 600);
-    h_eta->Draw();
-    c_eta->Update();
-    c_eta->SaveAs((outdir + "DiMuonEta.png").c_str());
+    //Loop for diMuon kinematic variables histogram
 
-    //Histogram for azimuthal angle
+    for (int i = 0; i < 3; i++){
 
-    auto h_phi = new_df.Histo1D({"Phi", "Azimuthal_angle", 100, -3.14, 3.14}, "Muon_phi");
-    TCanvas *c_phi = new TCanvas("Phi", "Azimuthal_angle_canvas", 800, 600);
-    h_phi->Draw();
-    c_phi->Update();
-    c_phi->SaveAs((outdir + "DiMuonPhi.png").c_str());
+        TCanvas *c = new TCanvas((vars[i] + "canvas").c_str(), vars[i].c_str(), 800, 600);
 
+        auto h_1 = new_df.Histo1D(ROOT::RDF::TH1DModel(vars[i].c_str(), (vars[i] + to_string(1)).c_str(), 100 ,bounds[i].first, bounds[i].second), 
+                                  ("Muon" + to_string(0) + "_" + vars[i]).c_str());
+        auto h_2 = new_df.Histo1D(ROOT::RDF::TH1DModel(vars[i].c_str(), (vars[i] + to_string(2)).c_str(), 100 , bounds[i].first, bounds[i].second), 
+                                  ("Muon" + to_string(1) + "_" + vars[i]).c_str());
+
+        h_1->SetLineColor(kBlue);
+        h_2->SetLineColor(kRed);
+
+        h_1->Draw();
+        h_2->Draw("SAME");
+
+        c->Update();
+        c->SaveAs((outname + vars[i] + ".png").c_str());
+
+        delete c;
+    }
+
+
+    //Ending Chrono counter e elapsed time
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
+    //Print del tempo
+
+    std::cout << "Tempo di esecuzione: " << elapsed.count() << " secondi." << std::endl;
 
 }
